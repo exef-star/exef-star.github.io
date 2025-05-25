daada432a13f3342d0fa89cb9988739944e8c8de
Admin <2667174454@qq.com>
2025-05-25 17:29:42 +0800

    Site updated: 2025-05-25 17:29:38

---
+++ js/update.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function() {
+    new Vue({
+        data: function () {
+            this.$notify({
+                title: "当前版本为 1.16（页脚更新）🍵",
+                message: "🎂详情请见博客の更新日志！🍰",
+                position: 'top-left',
+                offset: 50,
+                showClose: true,
+                type: "success",
+                duration: 5000
+            });
+        }
+    })
+});
+

7c0e5c9922bac4f545c5fb1cd18cca021cf4fa05
Admin <2667174454@qq.com>
2025-06-08 17:13:04 +0800

    Site updated: 2025-06-08 17:12:55

--- js/update.js
+++ js/update.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     new Vue({
         data: function () {
             this.$notify({
-                title: "当前版本为 1.16（页脚更新）🍵",
+                title: "当前版本为 1.17（留言板更新）🍵",
                 message: "🎂详情请见博客の更新日志！🍰",
                 position: 'top-left',
                 offset: 50,

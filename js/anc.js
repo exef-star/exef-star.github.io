7b9218db7a4c5143d3d03ecae33af35a9e34ace2
Admin <2667174454@qq.com>
2025-07-20 17:49:51 +0800

    Site updated: 2025-07-20 17:49:46

--- js/anc.js
+++ js/anc.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
         data: function () {
             this.$notify({
                 title: "最新公告🍯",
-                message: "图床叒叒叒证书过期了。。。。<br>已经是第二次了，导致博客的图片显示功能瘫痪<br>已经和图床的盆友联系了一下，不必担心<br>(。﹏。)<br>PS：修好啦o(*≧▽≦)ツ",
+                message: "开了个全球线路，使用了Cloudflare的Pages，可以在左上角的六个点中的线路找到！",
                 position: 'top-left',
                 offset: 50,
                 showClose: true,
--- js/update.js
+++ js/update.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     new Vue({
         data: function () {
             this.$notify({
-                title: "当前版本为 56w22a（实验性预览更新）🍵",
+                title: "当前版本为 1.17.1🍵",
                 message: "🎂详情请见博客の更新日志！🍰",
                 position: 'top-left',
                 offset: 50,
